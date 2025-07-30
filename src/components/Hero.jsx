e1743497eaf390e61c8206983c3d5365f715ab28
sehee <shdesign0820@gmail.com>
2025-07-30 17:42:34 +0900

    Add_Insta+MoodicVideo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ import './Hero.scss';
 
 const Hero = () => {
   return (
-    <section id="hero" className="hero-section" style={{ backgroundColor: '#f47a4b', color: '#78AAFF' }}>
+    <section id="hero" className="hero-section">
       <div className="video-hero">
         <video
           autoPlay
@@ -15,17 +15,17 @@ const Hero = () => {
           className="background-video"
           /* poster="/img/123(14).jpg" */
         >
-          <source src="/video/ILLIT_DoTheDance.mp4" type="video/mp4" />
+          <source src="/video/Moodic_Video2.mp4" type="video/mp4" />
           
         </video>
 
         <div className="main-container">
           <div className="master-hero-section">
-            <div className="logo-wrap-hero">
+            {/* <div className="logo-wrap-hero">
               <img src="/img/Moodic_Logo.png" alt="Moodic Logo" className="logo-hero" />
-            </div>
+            </div> */}
 
-            <div className="bottom-tile-hero">
+            {/* <div className="bottom-tile-hero">
               <div className="byq-logo">
                 <div className="icon-sparkle">✨</div>
                 <div className="label">Moodic Studio</div>
@@ -42,7 +42,7 @@ const Hero = () => {
                 <div className="label">2025</div>
                 <div className="icon-sparkle">🌟</div>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,45 +1,28 @@
 import React, { useState, useRef } from 'react';
-import { TbPlayerTrackNextFilled } from "react-icons/tb";
-import { TbPlayerTrackPrevFilled } from "react-icons/tb";
+import { TbPlayerTrackNextFilled, TbPlayerTrackPrevFilled } from "react-icons/tb";
 import { BsShuffle } from "react-icons/bs";
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectCards, Autoplay } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/effect-cards';
-import { EffectCards, Autoplay } from 'swiper/modules';
 import './main.scss';
 
-const images = [
-  'img/home_img/CD.jfif','img/home_img/various.jfif',
-  'img/home_img/ment1.png','img/home_img/ment2.png',
-  'img/home_img/hand.jfif','img/home_img/drink.jfif',
-  'img/home_img/gitar.jfif','img/home_img/headset.jpg',
-];
+import albumData from '../data/MainData';
 
 const Main = () => {
   const swiperRef = useRef(null);
+  const [selectedAlbum, setSelectedAlbum] = useState(albumData[0]);
 
-  // 다음 슬라이드
   const handleNext = () => {
-    if (swiperRef.current) {
-      swiperRef.current.swiper.autoplay.stop();
-      swiperRef.current.swiper.slideNext();
-    }
+    swiperRef.current?.swiper.slideNext();
   };
 
-  // 이전 슬라이드
   const handlePrev = () => {
-    if (swiperRef.current) {
-      swiperRef.current.swiper.autoplay.stop();
-      swiperRef.current.swiper.slidePrev();
-    }
+    swiperRef.current?.swiper.slidePrev();
   };
 
-  // 리셋(처음으로 이동 후 자동재생 시작)
   const handleReset = () => {
-    if (swiperRef.current) {
-      swiperRef.current.swiper.slideTo(0);
-      swiperRef.current.swiper.autoplay.start();
-    }
+    swiperRef.current?.swiper.slideTo(0);
   };
 
   return (
@@ -49,37 +32,87 @@ const Main = () => {
         <div className="prevBtn" onClick={handlePrev}><TbPlayerTrackPrevFilled /></div>
         <div className="resetBtn" onClick={handleReset}><BsShuffle /></div>
       </div>
+
       <div className="mainTextImg">
         <div className="mainText">
-          <img src={process.env.PUBLIC_URL + 'img/home/home_text.png'} alt="" />
+          <img src={process.env.PUBLIC_URL + '/img/home/home_text.png'} alt="main-title" />
         </div>
+
         <div className="mainImg_slider">
           <Swiper
             effect={'cards'}
             grabCursor={true}
-            autoplay={{
-              delay: 2000,
-              disableOnInteraction: false,
-            }}
-            modules={[EffectCards, Autoplay]}
+            modules={[EffectCards]}
             className="mySwiper"
             ref={swiperRef}
           >
-            {images.map((img, idx) => (
-              <SwiperSlide key={idx}>
-                <img src={process.env.PUBLIC_URL + img} alt="" />
+            {albumData.map((album, idx) => (
+              <SwiperSlide className='albumImg' key={idx}>
+                <img
+                  src={process.env.PUBLIC_URL + '/' + album.img}
+                  alt={album.name}
+                  onClick={() => setSelectedAlbum(album)}
+                  style={{ cursor: 'pointer' }}
+                />
               </SwiperSlide>
             ))}
           </Swiper>
         </div>
       </div>
-      {/* 하단 무한 띠지 */}
+
+      {selectedAlbum && (
+        <div className="insta-wrapper">
+          {/* <img
+                className="insta-title-img"
+                src={process.env.PUBLIC_URL + '/img/instagram/insta.png'}
+                alt="instagram-title"
+              /> */}
+              <img
+                className="insta-logo"
+                src={process.env.PUBLIC_URL + '/img/instagram/insta_icon.png'}
+                alt="instagram-logo"
+              />
+          <img
+            className="corner-logo"
+            src={process.env.PUBLIC_URL + '/img/instagram/black_logo.png'}
+            alt="corner-logo"
+          />
+
+          <div className="insta-top">
+            <div className="insta-header">
+              
+            </div>
+          </div>
+
+          <div className="insta-bottom">
+            <Swiper
+              modules={[Autoplay]}
+              slidesPerView={4}
+              spaceBetween={20}
+              autoplay={{ delay: 2000, disableOnInteraction: false }}
+              loop={true}
+              className="insta-swiper"
+            >
+              {selectedAlbum.insta.map((img, index) => (
+                <SwiperSlide key={index}>
+                  <img
+                    src={process.env.PUBLIC_URL + `/img/instagram/${img}`}
+                    alt={`insta-${index}`}
+                    className="slide-img"
+                  />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+        </div>
+      )}
+
       <div className="bottom-banner">
         <div className="banner-track">
           {[...Array(10)].map((_, i) => (
             <img
               key={i}
-              src={process.env.PUBLIC_URL + 'img/belt/belt_black.png'}
+              src={process.env.PUBLIC_URL + '/img/belt/belt_black.png'}
               alt="banner"
               className="banner-img"
             />
